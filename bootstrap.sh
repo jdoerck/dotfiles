4bde6e7bc986f3fe93688cd7b65c2875e14b62ad
J Doerck <j.doerck@gmail.com>
2019-12-10 11:25:06 -0500

    adding .zsch

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo spctl --master-disable
+
+
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+
+
+curl -L -o ~/.oh-my-zsh/custom/themes/materialshell.zsh-theme https://raw.githubusercontent.com/carloscuesta/materialshell/master/materialshell.zsh
+
+https://github.com/mbadolato/iTerm2-Color-Schemes
+
+https://www.cyberciti.biz/faq/bash-shell-display-only-hidden-dot-files/
