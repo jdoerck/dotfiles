7304e576e8b0d4a3f7557ad75efdfed709f5021a
J Doerck <j.doerck@gmail.com>
2020-01-10 14:18:28 -0500

    adding jekyll more aliases

--- app/jekyll.zsh
+++ app/jekyll.zsh
@@ -1,3 +1,19 @@
 jekyllServe() {
   bundle exec jekyll serve -V
 }
+
+jekyllDoctor() {
+  bundle exec jekyll doctor
+}
+
+jekyllClean() {
+  bundle exec jekyll clean
+}
+
+jekyllNew() {
+  bundle exec jekyll new $1 --blank
+}
+
+jekyllNewTheme() {
+  bundle exec jekyll new-theme
+}
